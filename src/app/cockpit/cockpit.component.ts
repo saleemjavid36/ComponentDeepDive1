29aaf249c76ff0b4fcdab0f6b149add315ba0763
saleemjavid36 <saleemjavid360@gmail.com>
2023-01-10 10:20:19 +0530

    dataBindingDeepDive

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit,EventEmitter, Output } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-  @Output() serverCreated: any=new EventEmitter<{serverName:string,serverContent:string}>();
-  @Output() bluePrintCreated: any=new EventEmitter<{serverName:string,serverContent:string}>();
+  @Output('srCreated') serverCreated: any=new EventEmitter<{serverName:string,serverContent:string}>();
+  @Output('bpCreated') bluePrintCreated: any=new EventEmitter<{serverName:string,serverContent:string}>();
   newServerName='';
   newServerContent='';
   
